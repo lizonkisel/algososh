f8970828a9f0b30c11fadd4543339c082c3db48e
lizonkisel <ees.hwork@gmail.com>
2022-12-27 19:34:46 +0300

    feat: test function reverseString. Start working on animation

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -6,9 +6,9 @@ import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
-export const StringComponent: React.FC = () => {
+import { ElementStates } from "../../types/element-states";
 
-  const [text, setText] = React.useState('');
+export const StringComponent: React.FC = () => {
 
   const initialState = { 
     text: '',
@@ -16,37 +16,109 @@ export const StringComponent: React.FC = () => {
     isRotated: false
   };
 
+  const [lettersState, setLettersState] = React.useState<{letter: string, state: ElementStates}[]>([]);
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
   function reducer(state: any, action: any) {
     switch (action.type) {
-      case 'start':
+
+      
+
+      case 'add_letters':
         return {
           ...state,
-          text: text,
+          text: action.value,
+          isRotateStarted: false
+        };
+      case 'set_changing':
+        return {
+          
+        }
+      case 'set_modified':
+        return {
+
+        }
+      case 'start':
+
+        console.log(reverseString(lettersState));
+
+        return {
+          ...state,
           isRotateStarted: true,
         }
       default:
         throw new Error(`Wrong type of action: ${action.type}`);
     }
-  }
+  };
 
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  // function reverseString(initialString: string) {
+  //   const initialArray = initialString.split('');
+  //   let i = 0;
+  //   let j = initialArray.length - 1;
+  //   while (i < j ) {
+  //     let start = initialArray[i];
+  //     let end = initialArray[j];
+
+
+  //     initialArray[i] = end;
+  //     initialArray[j] = start;
+
+  //     i++;
+  //     j--;
+  //   }
+
+  //   return initialArray.join('');
+  // };
+
+  function reverseString(lettersState: any) {
+    // const initialArray = initialString.split('');
+    console.log(lettersState);
+
+    let i = 0;
+    let j = lettersState.length - 1;
+    while (i < j ) {
+      let start = lettersState[i];
+      console.log(start);
+      let end = lettersState[j];
+
+      setTimeout(start.state = ElementStates.Changing, 2000);
+      setTimeout(end.state = ElementStates.Changing, 2000);
+
+      lettersState[i] = end;
+      lettersState[j] = start;
+
+      setTimeout(start.state = ElementStates.Modified, 2000);
+      setTimeout(end.state = ElementStates.Modified, 2000);
+
+      i++;
+      j--;
+    }
+
+    return lettersState;
+  };
 
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
     console.log('Submit');
 
+    const lettersArray = state.text.split('');
+    const newlettersArray = lettersArray.map((letter: string) => {
+      return {letter: letter, state: ElementStates.Default}
+    });
+
+    setLettersState([...newlettersArray]);
+
     dispatch({ type: 'start' });
 
-    // text.split('').map((element) => {
-    //   return <Circle letter={element}/>
-    // });
+    // console.log(reverseString(state.text));
+
+    // console.log(reverseString(lettersState));
 
   };
 
   function handleChange(e: React.FormEvent<HTMLInputElement>): void {
-    setText(e.currentTarget.value);
-    console.log(text);
+    dispatch({ type: 'add_letters', value: e.currentTarget.value });
   };
 
 
@@ -63,13 +135,12 @@ export const StringComponent: React.FC = () => {
       {
         state.isRotateStarted && 
         <section className={styles.bubbles}>
-          <div>Let's start</div>
+          {lettersState.map((letter: any) => {
+            return <Circle letter={letter.letter} state={letter.state}/>
+          })}
+          {/* <div>{state.text}</div> */}
         </section>
       }
-        
-      {/* <section className={styles.bubbles}>
-
-      </section> */}
 
     </SolutionLayout>
   );
