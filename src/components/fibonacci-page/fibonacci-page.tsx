72d682450ef4fc7d4d3d18e6253ceabe99fcd6d1
lizonkisel <ees.hwork@gmail.com>
2023-01-11 13:32:27 +0300

    feat: add form and dunction for Fibonacci row

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,44 @@
 import React from "react";
+import styles from "./fibonacci-page.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 
 export const FibonacciPage: React.FC = () => {
+
+  const initialState = {
+    isRotateStarted: false,
+    isRotated: false
+  };
+
+  function getFibonacciRow(n: number): number[] {
+    const arr = [1, 1];
+    console.log('azaza');
+    if (n === 1) {
+      return arr;
+    }
+    for (let i = 2; i < n + 1; i++) {
+      const value = arr[i - 2] + arr[i - 1];
+      arr.push(value);
+    }
+    return arr;
+  };
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log('Submit');
+  };
+
+  console.log(getFibonacciRow(1));
+
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+     <form className={styles.inputField__wrapper} onSubmit={handleSubmit}>
+        <Input isLimitText={true} max={19} min={1} extraClass={styles.inputField__input} type='number'/>
+        <Button text='Развернуть' type='submit'/>
+      </form>
     </SolutionLayout>
   );
 };
