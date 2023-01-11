27b4b2fb3c386f67081388f2193ccfda6ddfe2e9
lizonkisel <ees.hwork@gmail.com>
2023-01-11 12:22:28 +0300

    refactor: initial commit for branch month-12/step-1

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,7 +4,7 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      {/* Initial */}
     </SolutionLayout>
   );
 };
