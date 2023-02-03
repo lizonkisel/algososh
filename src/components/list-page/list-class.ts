12fa1f0770a34324538e978ff178f2b7985ac65d
lizonkisel <ees.hwork@gmail.com>
2023-02-03 12:40:10 +0300

    feat: create initial design with circles for ListPage

---
+++ src/components/list-page/list-class.ts
@@ -0,0 +1,109 @@
+export class Node<T> {
+    value: T
+    next: Node<T> | null
+    constructor(value: T, next?: Node<T> | null) {
+        this.value = value;
+        this.next = (next === undefined ? null : next);
+    }
+}
+  
+interface ILinkedList<T> {
+    append: (element: T) => void;
+    getSize: () => number;
+    print: () => void;
+}
+  
+export class LinkedList<T> implements ILinkedList<T> {
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
+    private size: number;
+    private list: any;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        this.list = new Node(0);
+    }
+  
+    append(element: T) {
+      const node = new Node(element);
+      if (this.head === null || this.tail === null) {
+        this.head = node;
+        this.tail = node;
+      } else {
+        this.tail.next = node;
+        this.tail = node;
+      }
+      console.log(node);
+      
+      this.size++;
+    }
+  
+    getSize() {
+      return this.size;
+    }
+
+    // addToList() {
+
+    // }
+
+    // getList() {
+
+    //     let dummyHead = new Node(0); // добавим в начало пустой узел
+    //     //@ts-ignore
+    //     dummyHead.next = this.head;
+    //     let curr = dummyHead;
+               
+    //     let prev;
+        
+    //     while (curr && curr.next) {
+
+    //     //   prev = curr;
+    //     //   curr = curr.next;
+    //     //   while (curr.value === val) {
+    //     //     if (curr.next) {
+    //     //       prev.next = curr.next;
+    //     //       curr = curr.next;
+    //     //       console.log('Here');
+    //     //     } else {
+    //     //       console.log('azaza');
+    //     //       curr = prev;
+    //     //       curr.next = null;
+    //     //     }
+    //     //   } 
+    //     //   console.log(prev);
+    //     //   console.log(curr);
+    //     }
+    //     console.log(dummyHead.next);
+        
+    //     return dummyHead.next; 
+    // }
+  
+    print() {
+      let curr = this.head;
+      let res = '';
+      while (curr) {
+        res += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(res);
+    }
+
+    getArray() {
+        let curr = this.head;
+        let res = [];
+        while (curr) {
+        //   res += `${curr.value} `;
+            res.push(curr.value);
+            curr = curr.next;
+        }
+        console.log(res);
+        return res;
+      }
+  }
+  
+//   const list = new LinkedList<number>();
+//   list.append(12);
+//   list.append(13);
+//   list.append(14);
+//   list.print(); //4 12 13 1
--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -5,10 +5,56 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
 
 import { ElementStates } from "../../types/element-states";
 
+import { LinkedList } from "./list-class";
+
+const list = new LinkedList<number | string>();
+
 export const ListPage: React.FC = () => {
+
+  const [currentList, setCurrentList] = React.useState<{
+    // elem_value: number | string,
+    elem_value: any, 
+    state: ElementStates, 
+    index: number,
+    isHead: boolean, 
+    isTail: boolean, 
+  }[]>([]);
+
+  function createStartList() {
+    list.append(0);
+    list.append(34);
+    list.append(8);
+    list.append(1);
+
+    setCurrentList([...list.getArray().map((value, i) => {
+      return {elem_value: value, state: ElementStates.Default, index: i, isHead: i === 0 ? true : false, isTail: i === list.getSize() - 1 ? true : false}
+    })]);
+  };
+
+  React.useEffect(() => {
+    createStartList();
+  }, []);
+
+  const renderList = currentList.map((value, i) => {
+    return (
+      <div className={styles.listArea__list}>
+        <Circle 
+          letter={value.elem_value} 
+          state={ElementStates.Default} 
+          index={i} 
+          head={value.isHead ? 'head' : ''} 
+          tail={value.isTail ? 'tail' : ''} 
+        />
+        { i !== list.getSize() - 1 && <ArrowIcon /> }
+          
+      </div>
+    )
+  });
+
   return (
     <SolutionLayout title="Связный список">
       <form action="" className={styles.form}>
@@ -26,8 +72,8 @@ export const ListPage: React.FC = () => {
           <Button extraClass={styles.form__button_size_large} text='Удалить по индексу' type='button' name='delete_by_index'/>
         </fieldset>
 
-        <section className={styles.stackArea}>
-          {/* {renderStack} */}
+        <section className={styles.listArea}>
+          {renderList}
         </section>
       </form>
     </SolutionLayout>
