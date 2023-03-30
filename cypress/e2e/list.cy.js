2afb0ebae4837bf69396c882cbcb8447584b917f
lizonkisel <ees.hwork@gmail.com>
2023-03-30 16:39:58 +0300

    feat: add cypress tests for adding element to head and to tail for ListPage

--- cypress/e2e/list.cy.js
+++ cypress/e2e/list.cy.js
@@ -46,11 +46,48 @@ describe('Страница "Очередь"', function() {
     });
 
     it('Проверка  добавления элемента в head', () => {
+        cy.get('.text_type_input').eq(0).type(1);
+        cy.contains('button', 'Добавить в head').click();
 
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(0).children().first().invoke('attr', 'class').should('include', 'circle_content_');
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(0).children().first().children().eq(1).should('have.text', 1);
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(0).children().first().children().eq(1).invoke('attr', 'class').should('include', 'circle_changing_');
+
+        cy.wait(SHORT_DELAY_IN_MS);
+        cy.get('*[class^="circle_circle_"]').eq(0).invoke('attr', 'class').should('include', 'circle_modified_');
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(0).should('have.text', 'head');
+        cy.get('*[class^="circle_content_"]').get('.text_type_circle').eq(0).should('have.text', '1');
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(2).should('have.text', '0');
+
+        cy.get('*[class^="circle_content_"]').eq(1).children().eq(0).should('have.text', '');
+
+        cy.wait(SHORT_DELAY_IN_MS);
+        cy.get('*[class^="circle_circle_"]').eq(0).invoke('attr', 'class').should('include', 'circle_default_');
+
+        cy.get('section[class^="list-page_listArea_"]').children().should('have.length', 5);
     });
 
     it('Проверка добавления элемента в tail', () => {
+        cy.get('.text_type_input').eq(0).type(5);
+        cy.contains('button', 'Добавить в tail').click();
 
+        cy.get('*[class^="circle_content_"]').eq(3).children().first().children().first().invoke('attr', 'class').should('include', 'circle_content_');
+        cy.get('*[class^="circle_content_"]').eq(3).children().first().children().first().children().eq(1).should('have.text', 5);
+        cy.get('*[class^="circle_content_"]').eq(3).children().first().children().first().children().eq(1).invoke('attr', 'class').should('include', 'circle_changing_');
+
+        cy.wait(SHORT_DELAY_IN_MS);
+        cy.get('*[class^="circle_circle_"]').last().invoke('attr', 'class').should('include', 'circle_modified_');
+        cy.get('*[class^="circle_content_"]').last().children().last().should('have.text', 'tail');
+        cy.get('*[class^="circle_content_"]').get('.text_type_circle').last().should('have.text', '5');
+        cy.get('*[class^="circle_content_"]').last().children().eq(2).should('have.text', '4');
+        cy.get('*[class^="circle_content_"]').last().children().eq(3).should('have.text', 'tail');
+
+        cy.get('*[class^="circle_content_"]').eq(3).children().eq(3).should('have.text', '');
+
+        cy.wait(SHORT_DELAY_IN_MS);
+        cy.get('*[class^="circle_circle_"]').eq(0).invoke('attr', 'class').should('include', 'circle_default_');
+
+        cy.get('section[class^="list-page_listArea_"]').children().should('have.length', 5);
     });
 
     it('Проверка добавления элемента по индексу', () => {
