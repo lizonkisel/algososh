a4d85c6482b8e14b53296e0dd38cf4e8a42499cf
lizonkisel <ees.hwork@gmail.com>
2023-03-30 13:52:25 +0300

    feat: add cypress tests for default list rendering for ListPage

--- cypress/e2e/list.cy.js
+++ cypress/e2e/list.cy.js
@@ -32,5 +32,40 @@ describe('Страница "Очередь"', function() {
         });
     });
 
+    it('Проверка отрисовки дефолтного списка', () => {
+        const defaultList = [0, 34, 8, 1];
 
+        cy.get('*[class^="circle_content_"]').eq(0).children().eq(0).should('have.text', 'head');
+        cy.get('.text_type_circle').each((val, index) => {
+            cy.wrap(val).should('have.text', defaultList[index]);
+        });
+        cy.get('*[class^="circle_content_"]').each((val, index) => {
+            cy.wrap(val).children().eq(2).should('have.text', index);
+        });
+        cy.get('*[class^="circle_content_"]').eq(3).children().eq(3).should('have.text', 'tail');
+    });
+
+    it('Проверка  добавления элемента в head', () => {
+
+    });
+
+    it('Проверка добавления элемента в tail', () => {
+
+    });
+
+    it('Проверка добавления элемента по индексу', () => {
+
+    });
+
+    it('Проверка удаления элемента из head', () => {
+
+    });
+
+    it('Проверка удаления элемента из tail', () => {
+
+    });
+
+    it('Проверка удаления элемента по индексу', () => {
+
+    });
 });
